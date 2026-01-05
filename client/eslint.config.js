b1e5487055588c810bc22f8ff9e2a47efb75c706
Jishnu M <149863453+jishnu-m-nair@users.noreply.github.com>
2026-01-05 16:17:54 +0530

    Initial front-end setup and tooling
    * chore(client): initial setup and tailwindcss configuration
    
    * chore(client): add eslint and prettier configuration
    
    * chore(client): frontend env configuration
    
    * chore(client): axios configuration
    
    * chore(client): redux store basic setup

---
+++ client/eslint.config.js
@@ -0,0 +1,40 @@
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import react from 'eslint-plugin-react';
+import tseslint from 'typescript-eslint';
+import { defineConfig, globalIgnores } from 'eslint/config';
+
+export default defineConfig([
+  globalIgnores(['dist']),
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [
+      js.configs.recommended,
+      tseslint.configs.recommended,
+      react.configs.flat.recommended,
+      reactHooks.configs.flat.recommended,
+      reactRefresh.configs.vite,
+    ],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      globals: globals.browser,
+      parserOptions: {
+        ecmaFeatures: {
+          jsx: true,
+        },
+      },
+    },
+
+    settings: {
+      react: {
+        version: 'detect',
+      },
+    },
+
+    rules: {
+      'react/react-in-jsx-scope': 'off', // React 17+
+    },
+  },
+]);

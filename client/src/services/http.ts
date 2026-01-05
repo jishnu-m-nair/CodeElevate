26b420a5e66234de81af6f42531a5fef00c321f2
Jishnu M <mjishnu2k02@gmail.com>
2026-01-05 16:05:44 +0530

    chore(client): redux store basic setup

--- client/src/services/http.ts
+++ client/src/services/http.ts
@@ -1,18 +1,18 @@
-import axios from "axios";
-import { env } from "../config/env";
+import axios from 'axios';
+import { env } from '../config/env';
 
 const http = axios.create({
   baseURL: env.API_BASE_URL,
   withCredentials: true,
   timeout: 10000,
   headers: {
-    "Content-Type": "application/json",
+    'Content-Type': 'application/json',
   },
 });
 
 http.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("accessToken");
+    const token = localStorage.getItem('accessToken');
 
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
@@ -27,7 +27,7 @@ http.interceptors.response.use(
   (response) => response,
   (error) => {
     if (error.response?.status === 401) {
-      console.log(error)
+      console.log(error);
     }
     return Promise.reject(error);
   }
